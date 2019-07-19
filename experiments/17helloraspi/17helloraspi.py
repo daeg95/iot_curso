01e2e721f17ee5b0dec90c274a3d51f8c984d617
Daniela Embus <dembus@unicauca.edu.co>
2019-07-19 18:47:33 -0500

    add py file raspi

--- experiments/17helloraspi/17helloraspi.py
+++ experiments/17helloraspi/17helloraspi.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as gpio
+import time
 
 gpio.setmode(gpio.BOARD) # Iniciar la board
 gpio.setup(12, gpio.OUT) # Asignacion del pin
 gpio.output(12, True) # encender 
+time.sleep(12)
 gpio.output(12, False)
 gpio.cleanup(12)
 

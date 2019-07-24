9c95c103cb1ef45279411dc34f2b594aedfe0871
Daniela Embus <dembus@unicauca.edu.co>
2019-07-24 17:03:21 -0500

    17helloraspi

--- experiments/17helloraspi/17helloraspi.py
+++ experiments/17helloraspi/17helloraspi.py
@@ -2,10 +2,22 @@ import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BOARD) # Iniciar la board
-gpio.setup(12, gpio.OUT) # Asignacion del pin
-gpio.output(12, True) # encender 
-time.sleep(12)
-gpio.output(12, False)
-gpio.cleanup(12)
+gpio.setup(24, gpio.OUT) # Asignacion del pin
+
+gpio.setup(23, gpio.IN)
+
+# gpio.output(12, True) # encender 
+# time.sleep(12)
+# gpio.output(12, False)
+# gpio.cleanup(12)
+
+
+
+while True: 
+    input_state == GPIO.input(23)
+    if input_state == False:
+        gpio.output(24, True)
+    else:
+        gpio.output(24, False)
 
 
